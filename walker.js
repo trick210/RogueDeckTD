1c8639a923069df2a3b6cd8c859c41a709582385
trick210 <trick210@live.com>
2021-10-23 00:44:02 +0200

    Updated walker movement

--- walker.js
+++ walker.js
@@ -12,6 +12,9 @@ class Walker extends Entity {
     this.hp = 60;
 
     this.tileIndex = 0;
+
+    this.oldvx = 0;
+    this.oldvy = 0;
   }
 
   update() {
@@ -23,13 +26,19 @@ class Walker extends Entity {
       let vx = tiles[this.tileIndex + 1][0] - tiles[this.tileIndex][0];
       let vy = tiles[this.tileIndex + 1][1] - tiles[this.tileIndex][1];
 
+      if (vx != this.oldvx || vy != this.oldvy) {
+        this.x = tiles[this.tileIndex][0] * gridSize;
+        this.y = tiles[this.tileIndex][1] * gridSize;
+      }
+
+      this.oldvx = vx;
+      this.oldvy = vy;
+
       this.x += vx * this.speed * (deltaTime / 20);
       this.y += vy * this.speed * (deltaTime / 20);
 
       if (-vx * (this.x - tiles[this.tileIndex + 1][0] * gridSize) <= 0 && -vy * (this.y - tiles[this.tileIndex + 1][1] * gridSize) <= 0) {
         this.tileIndex++;
-        this.x = tiles[this.tileIndex][0] * gridSize;
-        this.y = tiles[this.tileIndex][1] * gridSize;
       }
 
     } else {
